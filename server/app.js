f0ac7864fc61b7ad1a3a58930588c861d9c61ae1
MNK-FTHR <victorgranier.pierre@gmail.com>
2023-03-31 10:20:09 +0200

    full rework
    je pensais faire un serveur node qui servais directement du React en passant des données mais visiblement on peut pas donc je fait une MERN stack classique, là j'ai juste fait le register

---
+++ server/app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const dbConnect = require("./db/dbConnect");
+const User = require("./db/userModel");
+
+dbConnect();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (request, response, next) => {
+  response.json({ message: "Hey! This is your server response!" });
+  next();
+});
+
+app.post("/register", (request, response) => {
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword,
+      });
+
+      user
+        .save()
+        .then((result) => {
+          response.status(201).send({
+            message: "User Created Successfully",
+            result,
+          });
+        })
+        .catch((error) => {
+          response.status(500).send({
+            message: "Error creating user",
+            error,
+          });
+        });
+    })
+    .catch((e) => {
+      response.status(500).send({
+        message: "Password was not hashed successfully",
+        e,
+      });
+    });
+});
+
+module.exports = app;
